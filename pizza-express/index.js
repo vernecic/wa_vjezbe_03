b42198cd0058da7d2d92e2554e8320839d48a8b2
vernecic <vidernecic1@gmail.com>
2026-01-18 17:33:26 +0100

    post narudzbe

--- pizza-express/index.js
+++ pizza-express/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import pizzeRouter from "./routes/pizze.js";
+import narudzbeRouter from "./routes/narudzbe.js";
 
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -11,6 +12,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use("/pizze", pizzeRouter);
+app.use("/narudzbe", narudzbeRouter);
 
 app.get("/", (req, res) => {
   res.send("Dobrodošli u Pizza Express poslužitelj");
--- pizza-express/routes/narudzbe.js
+++ pizza-express/routes/narudzbe.js
@@ -1,46 +1,72 @@
-// import express from "express";
-// import { narudzbe, pizze } from "../data/data.js";
-// const router = express.Router();
+import express from "express";
+import connectToDatabase from "../db.js";
 
-// router.post("/", (req, res) => {
-//   const { narucene_pizze, podaci_dostava } = req.body;
-//   if (narucene_pizze.length === 0 || !narucene_pizze) {
-//     return res.status(400).json({
-//       message: "Pizze nisu odabrane.",
-//     });
-//   }
-//   let ukupna_cijena = 0;
-//   for (const narucena of narucene_pizze) {
-//     const pizza = pizze.find(
-//       (p) => p.naziv.toLowerCase() === narucena.naziv.toLowerCase()
-//     );
-//     if (!pizza) {
-//       return res.status(404).json({
-//         messsage: "Pizza ne postoji",
-//       });
-//     }
-//     const velicina = narucena.velicina.toLowerCase();
-//     const cijena = pizza.cijene[velicina];
-//     if (!cijena) {
-//       return res.status(400).json({
-//         message: `Veličina ${narucena.velicina} nije dostupna za pizzu ${narucena.naziv}.`,
-//       });
-//     }
-//     ukupna_cijena = ukupna_cijena + cijena * narucena.kolicina;
-//   }
-//   ukupna_cijena = Number(ukupna_cijena.toFixed(2));
+const router = express.Router();
 
-//   const nova_narudzba = {
-//     id: narudzbe.length + 1,
-//     narucene_pizze,
-//     ukupna_cijena,
-//     podaci_dostava,
-//   };
-//   narudzbe.push(nova_narudzba);
-//   res.status(201).json({
-//     message: "Narudzba uspjesno kreirana.",
-//     narudzba: nova_narudzba,
-//   });
-// });
+let db = await connectToDatabase();
 
-// export default router;
+router.post("/", async (req, res) => {
+  let narudzbe_collection = db.collection("narudzbe");
+  let pizza_collection = db.collection("pizze");
+  let novaNarudzba = req.body;
+  let ukupnaCijenaNarudzbe = 0;
+
+  let kljucevi = ["prezime", "adresa", "telefon", "narucene_pizze"];
+  let narudzbaKljucevi = Object.keys(novaNarudzba);
+  let kljuceviPizze = ["naziv", "kolicina", "velicina"];
+
+  let kljuceviSePodudaraju = kljucevi.every((k) =>
+    narudzbaKljucevi.includes(k),
+  );
+  if (!kljuceviSePodudaraju || kljucevi.length !== narudzbaKljucevi.length) {
+    return res.status(400).json({
+      message: "Ključevi se ne podudaraju",
+    });
+  }
+
+  for (let stavka of novaNarudzba.narucene_pizze) {
+    let pizza_kljucevi = Object.keys(stavka);
+    let kljuceviPizzeSePodudaraju = kljuceviPizze.every((k) =>
+      pizza_kljucevi.includes(k),
+    );
+    if (
+      !kljuceviPizzeSePodudaraju ||
+      pizza_kljucevi.length !== kljuceviPizze.length
+    ) {
+      return res.status(400).json({
+        message: "Ključevi naručenih pizza se ne podudaraju.",
+      });
+    }
+
+    let pizza = await pizza_collection.findOne({
+      naziv: stavka.naziv,
+    });
+    if (!pizza) {
+      return res.status(400).json({
+        message: "Pizza ne postoji,.",
+      });
+    }
+    let cijena = pizza.cijena[stavka.velicina];
+    let ukupnaCijenaPizze = cijena * stavka.kolicina;
+    ukupnaCijenaNarudzbe += ukupnaCijenaPizze;
+  }
+  if (isNaN(novaNarudzba.telefon)) {
+    return res.status(400).json({
+      message: "Broj telefon mora biti broj.",
+    });
+  }
+  novaNarudzba.ukupna_cijena = ukupnaCijenaNarudzbe;
+
+  try {
+    let result = await narudzbe_collection.insertOne(novaNarudzba);
+    res.status(200).json({
+      message: "Narudžba poslana",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  l;
+});
+
+export default router;
